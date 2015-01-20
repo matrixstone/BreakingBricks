905ebdd3c88650bf6a35b50f8fbca18b7483bb35
xuhe <xuhe@yahoo-inc.com>
2015-01-19 16:00:15 -0800

    Initial Commit

---
+++ BreakingBricks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BreakingBricks
+//
+//  Created by Xu He on 1/19/15.
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreakingBricks/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BreakingBricks
+//
+
+//  Copyright (c) 2015 Xu He. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
